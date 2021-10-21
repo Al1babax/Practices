e1f5665f7cbd1463a61845371a85ddb65e6a4abd
Alibaba <KPJ3tTUtjXjqvz4>
2021-10-21 22:21:08 +0300

    New

--- Homework/Al1baba/python workout book solutions/Exercise 8 Sorting a string.py
+++ Homework/Al1baba/python workout book solutions/Exercise 8 Sorting a string.py
@@ -1,7 +1,7 @@
 def str_sort(str1):
     char_dict = {x: ord(x) for x in str1}
 
-    print(sorted(char_dict))
-
+    sorted_by_value = {k: v for k, v in sorted(char_dict.items(), key=lambda v: v[1])}
+    print(sorted_by_value)
 
 str_sort("cbadfmgkldfjklgmkldfgmldfmkgig")
